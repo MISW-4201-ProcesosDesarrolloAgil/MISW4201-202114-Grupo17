8fee33f2641b65e6315c719c12009ea2d0afc472
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-09-05 10:51:32 -0500

    Se añaden modificaciones a archivo backtest.py

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -13,39 +13,114 @@ class TestIonicBack(unittest.TestCase):
         self.fkr = Faker()
         Faker.seed(4321)
         
-    """Crea varios usuarios y un album para ser compartido con usuarios distintos al que creo el album"""
-    def testCrearAlbumCompartirConUsuarios(self):
+    #CA01 y CA03 Crea varios usuarios y un album para ser compartido con usuarios distintos al que creo el album
+    def test_crear_album_compartir_con_usuarios(self):
         idUsuarios=[]
         idAlbums=[]
         tokens = []
+        #Crea 3 usuarios
         for i in range(4):
-            print()
             payload ={
                     "nombre":'{}'.format(self.fkr.name()),
                     "contrasena":"lola123"
                 }
-            print(payload)
-            response = self.app.post('/signIn',json=payload).get_json()
-            idUsuarioCreado = decode_token(encoded_token=response['token'])['sub']
+            responseUser = self.app.post('/signIn',json=payload).get_json()
+            idUsuarioCreado = decode_token(encoded_token=responseUser['token'])['sub']
             idUsuarios.append(idUsuarioCreado)
-            tokens.append(response['token'])
+            tokens.append(responseUser['token'])
             self.instancias.append(Usuario.query.get(idUsuarioCreado))
-        # for i in range(4):
-        #     fakeMedio=['DISCO','CASETE','CD']
-        #     albumPayload={
-        #         "titulo":self.fkr.name(),
-        #         "anio":self.fkr.year(),
-        #         "descripcion":self.fkr.lorem(),
-        #         "medio":self.fkr.arrayElement(fakeMedio)
-        #     }
-        #     response = self.app.post('/usuario/1/albumes',json=albumPayload).get_json()
-        #     self.instancias.append(Album.query.get(response['id']))
-        #     idAlbums.append(response['id'])
-        print(idUsuarios)
-        self.assertEqual('a','a')
+        #Crea 3 albums del usuario 1
+        for i in range(4):
+            fakeMedio=['DISCO','CASETE','CD']
+            albumPayload={
+                "titulo":self.fkr.name(),
+                "anio":self.fkr.year(),
+                "descripcion": self.fkr.paragraph(nb_sentences=5),
+                "medio":fakeMedio[self.fkr.random_int(min=0,max=2)]
+            }
+            responseAlbum = self.app.post(
+                '/usuario/1/albumes',
+                json=albumPayload,
+                headers={
+                    'Authorization':'Bearer {}'.format(tokens[0])
+                }
+                ).get_json()
+            self.instancias.append(Album.query.get(responseAlbum['id']))
+            idAlbums.append(responseAlbum['id'])
+        
+        #Comparte el Album 1 con los usuarios 2 y 3
+        responseAlbumCompartido = self.app.put(
+            '/usuario/1/album/1',
+            json={
+                'usuarioscompartidos':[2,3]
+            },
+            headers={
+                'Authorization':'Bearer {}'.format(tokens[0])
+            }
+        ).get_json()
+        usuariosCompartidosAlbumUno = Album.query.get(responseAlbumCompartido['id']).usuarioscompartidos
+
+        self.assertEqual(usuariosCompartidosAlbumUno[0].id,2)
+        self.assertEqual(usuariosCompartidosAlbumUno[1].id,3)
+    #CA04 verifica que cuando un usuario tenga albumes creados y compartidos, al pedir los albumes del usuario, se retorne 1 array concatenado de albumes creados + compartidos
+    def test_retorna_albums_propios_y_compartidos(self):
+        idUsuarios=[]
+        idAlbums=[]
+        tokens = []
+        #Crea 3 usuarios
+        for i in range(4):
+            payload ={
+                    "nombre":'{}'.format(self.fkr.name()),
+                    "contrasena":"lola123"
+                }
+            responseUser = self.app.post('/signIn',json=payload).get_json()
+            idUsuarioCreado = decode_token(encoded_token=responseUser['token'])['sub']
+            idUsuarios.append(idUsuarioCreado)
+            tokens.append(responseUser['token'])
+            self.instancias.append(Usuario.query.get(idUsuarioCreado))
+        #Crea 2 albums del usuario 1 y usuario 2 respectivamente
+        for i in range(2):
+            fakeMedio=['DISCO','CASETE','CD']
+            albumPayload={
+                "titulo":self.fkr.name(),
+                "anio":self.fkr.year(),
+                "descripcion": self.fkr.paragraph(nb_sentences=5),
+                "medio":fakeMedio[self.fkr.random_int(min=0,max=2)]
+            }
+            responseAlbum = self.app.post(
+                '/usuario/{}/albumes'.format(i+1),
+                json=albumPayload,
+                headers={
+                    'Authorization':'Bearer {}'.format(tokens[i])
+                }
+                ).get_json()
+            self.instancias.append(Album.query.get(responseAlbum['id']))
+            idAlbums.append(responseAlbum['id'])
+        
+        #Comparte el Album 1 con los usuarios 2 y 3
+        self.app.put(
+            '/usuario/1/album/1',
+            json={
+                'usuarioscompartidos':[2,3]
+            },
+            headers={
+                'Authorization':'Bearer {}'.format(tokens[0])
+            }
+        ).get_json()
+
+        #Obtiene los albums del usuario 2
+        responseAlbumsUsuario = self.app.get(
+                '/usuario/2/albumes',
+                json=albumPayload,
+                headers={
+                    'Authorization':'Bearer {}'.format(tokens[1])
+                }
+            ).get_json()
+        self.assertEqual(len(responseAlbumsUsuario),2)
+        
+        
 
     def tearDown(self) -> None:
         for i in self.instancias:
             db.session.delete(i)
         db.session.commit()
-        print(Usuario.query.all())

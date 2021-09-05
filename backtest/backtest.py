dbf5258a4237ff28abb4dd49ac73b70150af3ff5
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-09-05 09:44:15 -0500

    Se crean varios usuarios en la prueba

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -10,19 +10,38 @@ class TestIonicBack(unittest.TestCase):
     def setUp(self) -> None:
         self.instancias = []
         self.app = app.test_client()
+        self.fkr = Faker()
+        Faker.seed(4321)
         
-    """Test """
-    def testPrueba(self):
-
-        payload ={
-                "nombre":"Edgar Luna1",
-                "contrasena":"lola123"
-            }
-        response = self.app.post('/signIn',json=payload).get_json()
-        id = decode_token(encoded_token=response['token'])['sub']
-        usuarioPrueba = Usuario.query.get(id)
-        print(usuarioPrueba.nombre)
-        self.instancias.append(usuarioPrueba)
+    """Crea varios usuarios y un album para ser compartido con usuarios distintos al que creo el album"""
+    def testCrearAlbumCompartirConUsuarios(self):
+        idUsuarios=[]
+        idAlbums=[]
+        tokens = []
+        for i in range(4):
+            print()
+            payload ={
+                    "nombre":'{}'.format(self.fkr.name()),
+                    "contrasena":"lola123"
+                }
+            print(payload)
+            response = self.app.post('/signIn',json=payload).get_json()
+            idUsuarioCreado = decode_token(encoded_token=response['token'])['sub']
+            idUsuarios.append(idUsuarioCreado)
+            tokens.append(response['token'])
+            self.instancias.append(Usuario.query.get(idUsuarioCreado))
+        # for i in range(4):
+        #     fakeMedio=['DISCO','CASETE','CD']
+        #     albumPayload={
+        #         "titulo":self.fkr.name(),
+        #         "anio":self.fkr.year(),
+        #         "descripcion":self.fkr.lorem(),
+        #         "medio":self.fkr.arrayElement(fakeMedio)
+        #     }
+        #     response = self.app.post('/usuario/1/albumes',json=albumPayload).get_json()
+        #     self.instancias.append(Album.query.get(response['id']))
+        #     idAlbums.append(response['id'])
+        print(idUsuarios)
         self.assertEqual('a','a')
 
     def tearDown(self) -> None:

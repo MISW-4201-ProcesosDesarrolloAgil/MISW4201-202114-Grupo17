0821b418332ab8af9d4f3b25e4c2b905552222a0
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-09-05 07:43:25 -0500

    Se añaden tests

---
+++ backtest/backtest.py
@@ -0,0 +1,32 @@
+  
+from flaskr.modelos import Usuario,Album,Cancion
+from faker import Faker
+from flaskr.app import db, app
+import unittest
+from flask_jwt_extended import decode_token
+
+
+class TestIonicBack(unittest.TestCase):
+    def setUp(self) -> None:
+        self.instancias = []
+        self.app = app.test_client()
+        
+    """Test """
+    def testPrueba(self):
+
+        payload ={
+                "nombre":"Edgar Luna1",
+                "contrasena":"lola123"
+            }
+        response = self.app.post('/signIn',json=payload).get_json()
+        id = decode_token(encoded_token=response['token'])['sub']
+        usuarioPrueba = Usuario.query.get(id)
+        print(usuarioPrueba.nombre)
+        self.instancias.append(usuarioPrueba)
+        self.assertEqual('a','a')
+
+    def tearDown(self) -> None:
+        for i in self.instancias:
+            db.session.delete(i)
+        db.session.commit()
+        print(Usuario.query.all())

29547861bcfeaf9526a74084d885087c5917c172
Gerzon <gerzon.bautista@dafiti.com.co>
2021-09-07 16:03:05 -0500

    Se agrega el sidebar a los componentes listar albumes y canciones

--- front-end/tutorial-canciones/src/app/album/album.module.ts
+++ front-end/tutorial-canciones/src/app/album/album.module.ts
@@ -6,13 +6,14 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { AlbumCreateComponent } from './album-create/album-create.component';
 import { AlbumEditComponent } from './album-edit/album-edit.component';
 import { AppHeaderModule } from '../app-header/app-header.module';
+import { AppSidebarModule } from '../app-sidebar/app-sidebar.module';
 import { AlbumJoinCancionComponent } from './album-join-cancion/album-join-cancion.component';
 
 
 @NgModule({
   declarations: [AlbumListComponent, AlbumDetailComponent, AlbumCreateComponent, AlbumEditComponent, AlbumJoinCancionComponent],
   imports: [
-    CommonModule, ReactiveFormsModule, AppHeaderModule
+    CommonModule, ReactiveFormsModule, AppHeaderModule, AppSidebarModule
   ],
   exports:[AlbumListComponent, AlbumDetailComponent, AlbumCreateComponent, AlbumEditComponent, AlbumJoinCancionComponent]
 })
--- front-end/tutorial-canciones/src/app/app-sidebar/app-sidebar.module.ts
+++ front-end/tutorial-canciones/src/app/app-sidebar/app-sidebar.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   declarations: [
     SidebarComponent

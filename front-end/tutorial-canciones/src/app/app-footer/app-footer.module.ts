317e967bceee582403ff88f73f9141aeda857606
Juan Carlos Nieto Z <jc.nieto@uniandes.edu.co>
2021-09-09 01:23:54 -0500

    Creacion de modulo y componente para footer

---
+++ front-end/tutorial-canciones/src/app/app-footer/app-footer.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppFooterComponent } from './app-footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AppFooterComponent]
+})
+export class AppFooterModule { }

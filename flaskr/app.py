e04449ef04272814e0ef21d8d608228353dbdc7d
Gerzon <gerzon.bautista@dafiti.com.co>
2021-09-16 23:01:22 -0500

    SE crean las relaciones para asociar los usuarios con canciones.

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,9 +1,8 @@
 from flaskr import create_app
 from flask_restful import Api
 from .modelos import db
-from .vistas import VistaUsuarios, VistaCanciones, VistaCancion, VistaSignIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum, VistaLogIn, VistaLogOut, VistaAlbumesCanciones,jwt
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS, cross_origin
+from .vistas import VistaUsuarios, VistaCancionesUsuario, VistaCancion, VistaSignIn, VistaAlbum, VistaAlbumesUsuario, VistaCancionesAlbum, VistaLogIn, VistaLogOut, VistaAlbumesCanciones, jwt
+from flask_cors import CORS
 
 app = create_app('default')
 app_context = app.app_context()
@@ -17,12 +16,12 @@ api = Api(app)
 api.add_resource(VistaSignIn, '/signIn')
 api.add_resource(VistaLogIn, '/logIn')
 api.add_resource(VistaLogOut, '/usuario/<int:id_usuario>/logOut')
-api.add_resource(VistaCanciones, '/usuario/<int:id_usuario>/canciones')
+api.add_resource(VistaUsuarios, '/usuarios')
+api.add_resource(VistaCancionesUsuario, '/usuario/<int:id_usuario>/canciones')
 api.add_resource(VistaCancion, '/usuario/<int:id_usuario>/cancion/<int:id_cancion>')
 api.add_resource(VistaAlbumesCanciones, '/usuario/<int:id_usuario>/cancion/<int:id_cancion>/albumes')
-api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
+api.add_resource(VistaAlbumesUsuario, '/usuario/<int:id_usuario>/albumes')
 api.add_resource(VistaAlbum, '/usuario/<int:id_usuario>/album/<int:id_album>')
 api.add_resource(VistaCancionesAlbum, '/usuario/<int:id_usuario>/album/<int:id_album>/canciones')
-api.add_resource(VistaUsuarios,'/usuarios')
 
 jwt.init_app(app)

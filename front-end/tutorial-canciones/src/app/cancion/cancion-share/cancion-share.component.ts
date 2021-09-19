0b2d174da32b1a54ca8d96db6e7f1ae8df18859a
jc-nieto <jc.nieto@uniandes.edu.co>
2021-09-19 10:00:12 -0500

    Se adiciona TS para modal compartir cancion

--- front-end/tutorial-canciones/src/app/cancion/cancion-share/cancion-share.component.ts
+++ front-end/tutorial-canciones/src/app/cancion/cancion-share/cancion-share.component.ts
@@ -20,23 +20,67 @@ export class CancionShareComponent implements OnInit {
   @Input() logUserId:number
   @Output() openModal= new EventEmitter
   @Output() quitShare = new EventEmitter
+
   constructor(private cs: CancionService, private cr: ActivatedRoute, private toastr:ToastrService) {
     this.selectedUsers=[]
     this.cancionId = cr.snapshot.params.cancionId
   }
 
-  ngOnInit() {
-
+  ngOnInit(): void {
+    this.obtenerUsuarios()
   }
 
-  terminarcompartir(){
-
-  }
-  compartirUsuarios(){
-
-  }
-  selectUser(i:number):void{
-
+  terminarcompartir():void
+  {
+    this.quitShare.emit()
   }
 
+  abrirModal()
+  {
+    this.openModal.emit()
+  }
+
+  obtenerUsuarios()
+  {
+    this.cs.getUsers(this.token).subscribe(usuariosApp=>
+      {
+        this.usuarios = usuariosApp
+        this.usuarioscompartidos.forEach(us=>
+          {
+            this.selectedUsers.push(us)
+          })
+        this.abrirModal()
+      })
+  }
+  selectUser(i:number):void
+  {
+    if(this.selectedUsers.includes(this.usuarios[i].id))
+    {
+      const selectIndex = this.selectedUsers.indexOf(this.usuarios[i].id)
+      this.selectedUsers.splice(selectIndex,1)
+    }
+    else{
+      this.selectedUsers.push(this.usuarios[i].id)
+    }
+  }
+  compartirUsuarios()
+  {
+    const albumIdInt = parseInt(this.cancionId)
+    this.cs.compartirCancion(
+      this.logUserId,
+      this.token,
+      albumIdInt,
+      this.selectedUsers
+      ).subscribe(cancion=>
+        {
+          this.toastr.success('Se compartio correctamente con los usuarios seleccionados.')
+          this.terminarcompartir()
+
+      },
+      error=>
+      {
+        this.toastr.error('Upsss.... algo salio mal. Vuelve a intentarlo mas adelante')
+      }
+      )
+  }
 }
